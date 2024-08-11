28b26c8f186f1283492ef060508b7e26f5c4595d
Aleji0309 <78771441+Aleji0309@users.noreply.github.com>
2024-08-11 14:00:09 -0600

    Add files via upload

---
+++ BlogCafe/js/dist/scripts.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// querySelector
+var heading = document.querySelector('.header__texto h2');
+console.log(heading); // querySslectorAll
+
+var enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = 'Nuevo Texto';
+enlaces[0].classList.add('nueva-clase'); //
+//# sourceMappingURL=scripts.dev.js.map
